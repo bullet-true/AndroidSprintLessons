28eebf9625a4d9bea781e61cdc88c52b3c921ed2
Igor Fedorov <bullet-1@yandex.ru>
2025-12-24 14:14:30 +0500

    Initial commit

---
+++ app/src/main/java/com/zerocool/androidsprintlessons/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.zerocool.androidsprintlessons.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+//Светлая тема
+val background = Color(0xFFF3EDF5)
+val mainTitleColor = Color(0xFF1E1E1E)
+val subtitleGrayColor = Color(0xFF8C8C8C)
+
+//Темная тема
+val backgroundDark = Color(0xFF121212)
+val mainTitleColorDark = Color(0xFFEAEAEA)
+val subtitleGrayColorDark = Color(0xFFD1D1D1)
---
+++ app/src/main/java/com/zerocool/androidsprintlessons/ui/theme/Type.kt
@@ -0,0 +1,45 @@
+package com.zerocool.androidsprintlessons.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.zerocool.androidsprintlessons.R
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    headlineLarge = TextStyle(
+        fontSize = 28.sp,
+        fontFamily = FontFamily(Font(R.font.gilroy_bold))
+    ),
+    headlineMedium = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = FontFamily(Font(R.font.gilroy_medium))
+    )
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+)
